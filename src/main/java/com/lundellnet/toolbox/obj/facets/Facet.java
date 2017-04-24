0e935e20d06a141eadc38a06546ba01dcd3a1e60
Nate Lundell <nate@lundellnet.com>
2017-04-24 12:14:23 -0500

    moved Facet to its own package

--- src/main/java/com/lundellnet/toolbox/obj/facets/Facet.java
+++ src/main/java/com/lundellnet/toolbox/obj/facets/Facet.java
@@ -15,7 +15,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-package com.lundellnet.toolbox.obj.parsers;
+package com.lundellnet.toolbox.obj.facets;
 
 import com.lundellnet.toolbox.obj.data_access.configs.DataAccessConf;
 
