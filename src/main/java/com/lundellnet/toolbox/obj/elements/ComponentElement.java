f2b65736534344ec0c342a5d48aeb2fbd0f92870
Nate Lundell <nate@lundellnet.com>
2017-04-25 14:11:19 -0500

    name change along with changes made in toolbox-api

--- src/main/java/com/lundellnet/toolbox/obj/elements/ComponentElement.java
+++ src/main/java/com/lundellnet/toolbox/obj/elements/ComponentElement.java
@@ -17,18 +17,18 @@
  */
 package com.lundellnet.toolbox.obj.elements;
 
-import com.lundellnet.toolbox.api.data_access.annotations.MatrixComponent;
+import com.lundellnet.toolbox.api.data_access.annotations.MatrixComponentPoint;
 import com.lundellnet.toolbox.obj.data_access.configs.DataAccessConf;
 import com.lundellnet.toolbox.obj.data_access.configurables.ConfigurableFieldAccess;
 
 public interface ComponentElement <I, O, C extends DataAccessConf<I, O>>
 	extends ConfigurableFieldAccess<I, O, C>, AnnotatedElement<I, O, C>
 {
-    default MatrixComponent getComponent() {
-	MatrixComponent component = getField().getAnnotation(MatrixComponent.class);
+    default MatrixComponentPoint getComponent() {
+	MatrixComponentPoint component = getField().getAnnotation(MatrixComponentPoint.class);
 	
 	if (component == null) {
-	    throw new DataPointElementException("No " + MatrixComponent.class.getName() + " associated with this Element.");
+	    throw new DataPointElementException("No " + MatrixComponentPoint.class.getName() + " associated with this Element.");
 	}
 	
 	return component;
